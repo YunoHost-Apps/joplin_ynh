e0c9ffef7daa69c8f6871cae5ade582fbaa40cf8
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-10-04 12:08:40 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-nodejs_version="22"
-
 function bcrypt_password() {
   echo -n "$1" | \
     python3 -c "import bcrypt; import sys; print(bcrypt.hashpw(bytes(sys.stdin.read(), 'ascii'), bcrypt.gensalt(rounds=10)).decode('ascii'))"
